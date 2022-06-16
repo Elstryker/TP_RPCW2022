9a0e6d85062471dd651d0f225ed79314e26bba8e
António Santos <antoniomsantos99@gmail.com>
2022-06-16 23:30:36 +0100

    Buttons fixed

--- app/routes/index.js
+++ app/routes/index.js
@@ -1,15 +1,17 @@
-var express = require('express');
+var express = require("express");
 var router = express.Router();
-var axios = require('axios');
+var axios = require("axios");
 
-var aux = require('./auxiliars')
+var aux = require("./auxiliars");
 
 /* GET home page. */
-router.get('/', function(req, res, next) {
-    if(!req.cookies.token) aux.consumerTokenGenerator(req.originalUrl, res)
-    else aux.renderIndex(req.cookies.token, res, {})
+router.get("/", function (req, res, next) {
+  if (!req.cookies.token) aux.consumerTokenGenerator(req.originalUrl, res);
+  else aux.renderIndex(req.cookies.token, res, {});
 });
 
-
+router.get("/recursos", function (req, res, next) {
+  res.render("recursos");
+});
 
 module.exports = router;
