0b90499ae4854672df412dbc961eb5073be29389
António Santos <antoniomsantos99@gmail.com>
2022-06-17 22:31:36 +0100

    Auto stash before merge of "main" and "origin/main"

--- api/routes/recursos.js
+++ api/routes/recursos.js
@@ -1,54 +1,54 @@
-var express = require('express');
+var express = require("express");
 var router = express.Router();
-const Recurso = require('../controllers/recurso')
-const TipoRecurso = require('../controllers/tipoRecurso')
+const Recurso = require("../controllers/recurso");
+const TipoRecurso = require("../controllers/tipoRecurso");
 
-
-
-router.get('/', function(req, res, next) {
-    if(req.query.tipo){
-        Recurso.pesquisarPorTipo(req.query.tipo, req.body.meus_recursos)
-        .then(dados => res.status(201).jsonp(dados))
-        .catch(e => res.status(508).jsonp({error: e}))
-    }
-    else if (req.query.a){
-        Recurso.pesquisarPorTitulo(req.query.a, req.body.meus_recursos)
-        .then(dados => res.status(201).jsonp(dados))
-        .catch(e => res.status(509).jsonp({error: e}))
-    }
-    Recurso.listar()
-        .then(dados => req.status(200).jsonp(dados))
-        .catch(e => res.status(501).jsonp({error: e}))
+router.get("/", function (req, res, next) {
+  if (req.query.tipo) {
+    Recurso.pesquisarPorTipo(req.query.tipo, req.body.meus_recursos)
+      .then((dados) => res.status(201).jsonp(dados))
+      .catch((e) => res.status(508).jsonp({ error: e }));
+  } else if (req.query.a) {
+    Recurso.pesquisarPorTitulo(req.query.a, req.body.meus_recursos)
+      .then((dados) => res.status(201).jsonp(dados))
+      .catch((e) => res.status(509).jsonp({ error: e }));
+  }
+  Recurso.listar()
+    .then((dados) => res.status(200).jsonp(dados))
+    .catch((e) => {
+      console.log(e);
+      res.status(501).jsonp({ error: e });
+    });
 });
 
-router.get('/:rid', function(req, res) {
-    Recurso.consultar(req.params.rid)
-      .then(dados => res.status(200).jsonp(dados))
-      .catch(e => res.status(504).jsonp({error: e}))
+router.get("/:rid", function (req, res) {
+  Recurso.consultar(req.params.rid)
+    .then((dados) => res.status(200).jsonp(dados))
+    .catch((e) => {
+      console.log(e);
+      res.status(504).jsonp({ error: e });
+    });
 });
-
-
 
 // Inserir recursos
-router.post('/', function(req, res){
-    Recurso.inserir(req.body.recurso)
-      .then(dados =>  res.status(201).jsonp({dados}))
-      .catch(e => res.status(506).jsonp({error: e}))
-})
+router.post("/", function (req, res) {
+  Recurso.inserir(req.body.recurso)
+    .then((dados) => res.status(201).jsonp({ dados }))
+    .catch((e) => res.status(506).jsonp({ error: e }));
+});
 
 // Alterar um recurso
-router.put('/', function(req, res){
-    Recurso.alterar(req.body)
-      .then(dados => res.status(201).jsonp({dados}))
-      .catch(e => res.status(507).jsonp({error: e}))
-})
+router.put("/", function (req, res) {
+  Recurso.alterar(req.body)
+    .then((dados) => res.status(201).jsonp({ dados }))
+    .catch((e) => res.status(507).jsonp({ error: e }));
+});
 
 //Incrementar numero de Downloads de um recurso
-router.post('/download', function(req, res){
-    Recurso.incrementarDownloads(req.body)
-    .then(dados => res.status(201).jsonp({dados}))
-    .catch(e => res.status(507).jsonp({error: e}))
-
-})
+router.post("/download", function (req, res) {
+  Recurso.incrementarDownloads(req.body)
+    .then((dados) => res.status(201).jsonp({ dados }))
+    .catch((e) => res.status(507).jsonp({ error: e }));
+});
 
 module.exports = router;
--- app/routes/recursos.js
+++ app/routes/recursos.js
@@ -1,108 +1,96 @@
-var express = require('express');
+var express = require("express");
 var router = express.Router();
-var axios = require('axios');
-var moment = require('moment')
-var multer = require('multer');
-var fs = require('fs');
-var mime = require('mime-types')
+var axios = require("axios");
+var moment = require("moment");
+var multer = require("multer");
+var fs = require("fs");
+var mime = require("mime-types");
 
-const pathLib = require('path')
+const pathLib = require("path");
 
-var aux = require('./auxiliars')
-const auxFiles = require('./funcs')
-var bagit = require('../public/javascripts/bagit');
-const { token } = require('morgan');
+var aux = require("./auxiliars");
+const auxFiles = require("./funcs");
+var bagit = require("../public/javascripts/bagit");
+const { token } = require("morgan");
 
-var upload = multer({dest: 'uploads/'})
+var upload = multer({ dest: "uploads/" });
 
+router.get("/", function (req, res) {
+  if (!req.cookies.token) aux.gerarTokenConsumidor(req.originalUrl, res);
+  else {
+    axios
+      .get("http://localhost:10000/api/recursos?token=" + req.cookies.token)
+      .then((dados) => {
+        res.render("recursos", dados);
+      })
+      .catch((error) => res.render("error", { error }));
+  }
+});
 
-router.get('/', function(req, res) {
-    if (!req.cookies.token) aux.gerarTokenConsumidor(req.originalUrl, res)
-    else {
-        axios.get('http://localhost:10000/api/recursos?token=' + req.cookies.token)
-            .then(dados => {
-                axios.get('http://localhost:10000/api/recursos/tipos?token=' + req.cookies.token)
-                    .then(tipos_bd => {
-                        var varsPug = aux.variaveisRecursos(dados.data, tipos_bd, req.cookies.token, false)
-                        res.render('recursos', varsPug)
-                    })
-                    .catch(error => res.render('error', {error}))
-        })
-        .catch(error => res.render('error', {error}))
+router.post("/file", upload.single("file"), async function (req, res) {
+  if (!req.cookies.token) auxFiles.gerarTokenConsumidor(req.originalUrl, res);
+  else {
+    console.log(req.body);
+    var token = aux.unveilToken(req.cookies.token);
+    let oldPath = pathLib.join(__dirname, "/../", req.file.path);
+    let newPath = pathLib.join(
+      __dirname,
+      "/../uploads/",
+      req.file.originalname + ".zip"
+    );
+
+    fs.renameSync(oldPath, newPath, function (err) {
+      if (err) throw err;
+    });
+
+    console.log(oldPath);
+    console.log(newPath);
+
+    console.log("File received");
+
+    try {
+      var files = auxFiles.loadZipAndProcessIt(newPath, req.body);
+    } catch (error) {
+      console.log(error);
     }
-})
 
-router.post('/file', upload.single('file'), async function(req, res) {
-    if(!req.cookies.token) auxFiles.gerarTokenConsumidor(req.originalUrl, res)
-    else {
-        console.log(req.body)
-        var token = aux.unveilToken(req.cookies.token)
-        let oldPath = pathLib.join(__dirname,'/../',req.file.path)
-        let newPath = pathLib.join(__dirname, '/../uploads/', req.file.originalname+'.zip')
+    var authors = req.body.authors.split(",");
+    var titles = req.body.titles.split(",");
+    var creationDates = req.body.creationDates.split(",");
 
-        fs.renameSync(oldPath, newPath, function(err) {
-            if(err)
-                throw err;
-        })
+    var autorRecurso = req.body.autorRecurso;
+    var tituloRecurso = req.body.tituloRecurso;
+    var descRecurso = req.body.descRecurso;
+    var tipoRecurso = req.body.tipoRecurso;
+    var dataRecurso = req.body.dataCriacao;
 
-        console.log(oldPath)
-        console.log(newPath)
+    var visibilidadeRecurso = true; //TODO: alterar
 
-        console.log("File received")
+    var filesList = [];
 
-        try {
-            var files = auxFiles.loadZipAndProcessIt(newPath,req.body)
-        } catch (error) {
-            console.log(error)
+    files
+      .then(function (files) {
+        console.log(files);
+
+        for (var i = 0; i < files.length; i++) {
+          var file = files[i];
+          var fileActualPath = pathLib.normalize(
+            __dirname.replace("/routes", "/public/files" + file)
+          );
+          var fileObj = {
+            creationDate: creationDates[i],
+            submissionDate: new Date().toISOString().substring(0, 16),
+            author: authors[i],
+            submitter: token.nivel,
+            title: titles[i],
+            mimetype: mime.lookup(fileActualPath),
+            size: fs.lstatSync(fileActualPath).size,
+            path: file,
+          };
+          console.log(fileObj);
+          filesList.push(fileObj);
         }
-
-        var authors = req.body.authors.split(',')
-        var titles = req.body.titles.split(',')
-        var creationDates = req.body.creationDates.split(',')
-        
-        var autorRecurso = req.body.autorRecurso
-        var tituloRecurso = req.body.tituloRecurso
-        var descRecurso = req.body.descRecurso
-        var tipoRecurso = req.body.tipoRecurso
-        var dataRecurso = req.body.dataCriacao
-
-        var visibilidadeRecurso = true; //TODO: alterar
-
-        var filesList = []
-
-        files.then(function (files) {
-            console.log(files)
-
-            for(var i = 0; i < files.length; i++) {
-                var file = files[i]
-                var fileActualPath = pathLib.normalize(__dirname.replace('/routes','/public/files' + file))
-                var fileObj = {
-                    creationDate: creationDates[i],
-                    submissionDate: new Date().toISOString().substring(0,16),
-                    author: authors[i],
-                    submitter: token.nivel,
-                    title: titles[i],
-                    mimetype: mime.lookup(fileActualPath),
-                    size: fs.lstatSync(fileActualPath).size,
-                    path: file,
-                }
-                console.log(fileObj);
-                filesList.push(fileObj)
-            }
-            var dataAtual = new Date().toISOString().substr(0,19)
-                
-            var recursoObj = {
-                idAutor: token._id,
-                nomeAutor: autorRecurso,
-                titulo: tituloRecurso,
-                tipo: tipoRecurso,
-                descricao: descRecurso,
-                dataCriacao: dataRecurso,
-                dataRegisto: dataAtual,
-                dataUltimaMod: dataAtual,
-                visibilidade: visibilidadeRecurso,
-                ficheiros: filesList
-            }
+        var dataAtual = new Date().toISOString().substr(0, 19);
 
             axios.post('http://localhost:10000/api/recursos?token=' + req.cookies.token, {recurso: recursoObj})
                 .then(recurso => {
@@ -145,51 +133,71 @@ router.post('/file', upload.single('file'), async function(req, res) {
                 })
                 .catch(error => res.render('error', {error}))
 
-        })
-        .catch(err => {
-            console.log(err)
-            res.status(500).render('error', { error: err })
-        })
+              var noticiaObj = {
+                idAutor: token._id,
+                nomeAutor: token.username,
+                recurso: {
+                  id: novoRecurso._id,
+                  titulo: novoRecurso.titulo,
+                  tipo: novoRecurso.tipo,
+                  estado: "Novo",
+                },
+                data: dataAtual,
+              };
 
-
-
-    }
-})
+              axios
+                .post(
+                  "http://localhost:10000/api/noticias?token=" +
+                    req.cookies.token,
+                  { noticia: noticiaObj }
+                )
+                .then((dados) => res.redirect("/perfil"))
+                .catch((error) => res.render("error", { error }));
+            }
+          })
+          .catch((error) => res.render("error", { error }));
+      })
+      .catch((err) => {
+        console.log(err);
+        res.status(500).render("error", { error: err });
+      });
+  }
+});
 
 // A testar
-router.post('/download', (req,res) => {
-    if (!req.cookies.token) aux.gerarTokenConsumidor(req.originalUrl, res)
-    else {   
-    axios.get('http://localhost:10000/recursos/:id' + req.params.id + 'token=' + req.cookies.token)
-        .then(recursoData => {
+router.post("/download", (req, res) => {
+  if (!req.cookies.token) aux.gerarTokenConsumidor(req.originalUrl, res);
+  else {
+    axios
+      .get(
+        "http://localhost:10000/recursos/:id" +
+          req.params.id +
+          "token=" +
+          req.cookies.token
+      )
+      .then((recursoData) => {
+        let titulo = recursoData.data.titulo;
+        let ficheiros = recursoData.data.ficheiros;
+        var zip = bagit.compressFiles(ficheiros);
 
-            let titulo = recursoData.data.titulo
-            let ficheiros = recursoData.data.ficheiros
-            var zip = bagit.compressFiles(ficheiros)
-            
-
-        axios.post('http://localhost:10000/recursos/download?token=' + req.cookies.token, req.params.id)
-            .then(() => {
+        axios
+          .post(
+            "http://localhost:10000/recursos/download?token=" +
+              req.cookies.token,
+            req.params.id
+          )
+          .then(() => {
             res.writeHead(200, {
-                "Content-Type": "application/zip",
-                "Content-Disposition": `attachment; filename=${titulo}.zip`,
-            })
-            res.write(zip)
-            res.end()
-            })
-            .catch(errors => res.render('error', {error:errors[0]}))
-        })
-        .catch(error => res.render('error', {error}))
-    }
-})
-
-
-
-
-
-
-
-
-
+              "Content-Type": "application/zip",
+              "Content-Disposition": `attachment; filename=${titulo}.zip`,
+            });
+            res.write(zip);
+            res.end();
+          })
+          .catch((errors) => res.render("error", { error: errors[0] }));
+      })
+      .catch((error) => res.render("error", { error }));
+  }
+});
 
 module.exports = router;
