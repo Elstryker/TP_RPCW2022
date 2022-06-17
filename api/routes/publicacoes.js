54aadec112fda80d93915011f0bab37d2665346c
Eduardo Silva <62120563+EduardoSilva088@users.noreply.github.com>
2022-06-17 23:48:47 +0100

    mais erros

--- api/routes/publicacoes.js
+++ api/routes/publicacoes.js
@@ -3,34 +3,34 @@ var router = express.Router();
 const Publicacao = require('../controllers/publicacao');
 
 router.get('/', function(req, res) {
-  Publicacao.listar()
-    .then(dados => res.status(200).jsonp(dados))
-    .catch(e => res.status(501).jsonp({error: e}))
+    Publicacao.listar()
+        .then(dados => res.status(200).jsonp(dados))
+        .catch(e => res.status(501).jsonp({error: e}))
 })
 
 router.get('/autor/:id', function(req,res){
-  Publicacao.pubsUtilizador(req.params.id)
-    .then(dados=>res.status(200).jsonp(dados))
-    .catch(e => res.status(502).jsonp({error: e}))
+    Publicacao.pubsUtilizador(req.params.id)
+        .then(dados=>res.status(200).jsonp(dados))
+        .catch(e => res.status(502).jsonp({error: e}))
 })
 
 router.get('/:id', function(req, res) {
-  Publicacao.consultar(req.params.id)
-    .then(dados => res.status(200).jsonp(dados))
-    .catch(e => res.status(503).jsonp({error: e}))
+    Publicacao.consultar(req.params.id)
+        .then(dados => res.status(200).jsonp(dados))
+        .catch(e => res.status(503).jsonp({error: e}))
 })
 
 router.post('/', function(req,res){
-  Publicacao.inserir(req.body)
-    .then(dados => res.status(201).jsonp(dados))
-    .catch(e => res.status(504).jsonp({error: e}))
+    Publicacao.inserir(req.body.pub)
+        .then(dados => res.status(201).jsonp(dados))
+        .catch(e => res.status(504).jsonp({error: e}))
 })
 
 // Atualizar estado para indisponível
 router.post('/atualizarEstado/:id', function(req, res){
     Publicacao.atualizarEstado(req.params.id, req.body.disp)
-      .then(dados => res.status(201).jsonp({dados}))
-      .catch(e => res.status(505).jsonp({error: e}))
+        .then(dados => res.status(201).jsonp({dados}))
+        .catch(e => res.status(505).jsonp({error: e}))
 })
 
 router.post('/comentar/:id', function(req,res){
--- api/routes/recursos.js
+++ api/routes/recursos.js
@@ -4,29 +4,29 @@ const Recurso = require("../controllers/recurso");
 const TipoRecurso = require("../controllers/tipoRecurso");
 
 router.get("/", function (req, res, next) {
-  if (req.query.tipo) {
+    if (req.query.tipo) {
     Recurso.pesquisarPorTipo(req.query.tipo, req.body.meus_recursos)
-      .then((dados) => res.status(201).jsonp(dados))
-      .catch((e) => res.status(508).jsonp({ error: e }));
-  } else if (req.query.a) {
+        .then((dados) => res.status(201).jsonp(dados))
+        .catch((e) => res.status(508).jsonp({ error: e }));
+    } else if (req.query.a) {
     Recurso.pesquisarPorTitulo(req.query.a, req.body.meus_recursos)
-      .then((dados) => res.status(201).jsonp(dados))
-      .catch((e) => res.status(509).jsonp({ error: e }));
-  }
-  Recurso.listar()
-    .then((dados) => res.status(200).jsonp(dados))
-    .catch((e) => {
-      console.log(e);
-      res.status(501).jsonp({ error: e });
-    });
+        .then((dados) => res.status(201).jsonp(dados))
+        .catch((e) => res.status(509).jsonp({ error: e }));
+    }
+    Recurso.listar()
+        .then((dados) => res.status(200).jsonp(dados))
+        .catch((e) => {
+            console.log(e);
+            res.status(501).jsonp({ error: e });
+        });
 });
 
 router.get("/:rid", function (req, res) {
   Recurso.consultar(req.params.rid)
     .then((dados) => res.status(200).jsonp(dados))
     .catch((e) => {
-      console.log(e);
-      res.status(504).jsonp({ error: e });
+        console.log(e);
+        res.status(504).jsonp({ error: e });
     });
 });
 
